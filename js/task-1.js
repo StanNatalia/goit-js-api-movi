1cc3a91a8beb4be7e4dc2d1e66972f617031cecf
Natalia Stan <popkids.ua@gmail.com>
2024-12-18 20:08:39 +0200

    start

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,55 @@
-const customer = {
-    username: "Mango",
-    balance: 24000,
-    discount: 0.1,
-    orders: ["Burger", "Pizza", "Salad"],
-    // Change code below this line
-    getBalance() {
-      return this.balance;
-    },
-    getDiscount() {
-      return this.discount;
-    },
-    setDiscount(value) {
-        this.discount = value;
-    },
-    getOrders() {
-      return this.orders;
-    },
-    addOrder(cost, order) {
-        this.balance -= cost - cost * this.discount;
-        this.orders.push(order);
-    },
-    // Change code above this line
-  };
-  
-  customer.setDiscount(0.15);
-  console.log(customer.getDiscount()); // 0.15
-  customer.addOrder(5000, "Steak");
-  console.log(customer.getBalance()); // 19750
-  console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const BASE_URL = "https://api.themoviedb.org/3";
+const END_POINT = "/trending/movie/week";
+const API_KEY = "345007f9ab440e5b86cef51be6397df1";
+
+const container = document.querySelector(".js-movie-list");
+const loadMore = document.querySelector(".js-load-more");
+
+loadMore.addEventListener("click", onLoadMore)
+
+let page = 1;
+
+async function serviceMovie(page = 1) {
+    const { data } = await axios(`${BASE_URL}${END_POINT}`, {
+      params: {
+        api_key: API_KEY,
+        page: page
+      }
+      });
+      return(data);
+}
+serviceMovie(page)
+  .then(data => {
+    console.log(data)
+    container.insertAdjacentHTML("beforeend", createMarkup(data.results))
+    loadMore.classList.replace("load-more-hidden", "load-more")
+  })
+  .catch(error => alert(error.message))
+
+
+  function createMarkup(arr) {
+     return arr.map(({ poster_path, release_date, original_title, vote_average }) => `
+     
+     <li class="movie-card">
+       <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}">
+       <div class="movie-info">
+          <h2>${original_title}</h2>
+          <p>Release Date: ${release_date}</p>
+          <p>Vote Average${vote_average}</p>
+       </div>
+     </li>
+     `).join("");
+  }
+
+  async function onLoadMore() {
+    page += 1;
+
+    try {
+
+      const data = await serviceMovie(page);
+      container.insertAdjacentHTML("beforeend", createMarkup(data.results))
+
+    } catch(error) {
+      alert(error.message)
+    }
+  }
